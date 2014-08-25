828c42db26b4921da174ff52061bc71ab51e16d0
Romi Halasz <novocainex@gmail.com>
2014-08-25 17:15:04 +0300

    Initial import.

---
+++ js/config/config.js
@@ -0,0 +1,9 @@
+var appURL = "http://localhost/web/hack";
+
+var mainMenuOptions = [
+	{label: "Home"},
+	{label: "Despre"},
+	{label: "Inscriere"},
+	{label: "Alte Chestii"},
+	{label: "Contact"}
+];
---
+++ js/section/main.js
@@ -0,0 +1,3 @@
+$( function() {
+	$("header").append(menuTemplate({options: mainMenuOptions}));
+})
